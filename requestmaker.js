5b601d41535ed6173275910ecda7df50f250f4aa
IanManuel BucetaSbrizza <IanManuel.BucetaSbrizza@Alumnos.uai.edu.ar>
2020-02-13 18:11:56 -0300

    Added

---
+++ requestmaker.js
@@ -0,0 +1,41 @@
+function requestMaker(baseURL){
+    var __baseURL = "http://localhost:65072";
+    var request = new XMLHttpRequest();
+
+    this.getStudentPrograms = function() {
+        request.open("GET", baseURL + "/api/StudentProgram", true);
+        request.send(null);
+    };
+
+    this.postStudentProgram = function(studentProgram) {
+        request.open("POST", baseURL + "/api/StudentProgram", true)
+        request.send(JSON.stringify())
+    };
+
+    this.patchStudentProgram = function(id, studentProgram) {
+
+        request.open("POST", baseURL + "/api/StudentProgram/" + id, true);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                var json = JSON.parse(request.responseText);
+                console.log();
+            }
+        };
+        var data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
+        request.send(data);
+    };
+    
+    this.deleteStudentProgram = function(id){
+        request.open("DELETE", baseURL + "/api/StudentProgram/", true);
+        request.send(id);
+    };
+
+    this.selectTopProgram = function(){
+        request.open("DELETE", baseURL + "/api/StudentProgram/Select", true);
+        request.send(null);
+    };
+    this.test = function(){
+        console.log("I'm stuff");
+    }
+}
