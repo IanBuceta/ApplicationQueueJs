fd50f1ce4c83565730a222813a606eddf1820dfe
IanBuceta <iansbrizza@gmail.com>
2020-02-16 14:05:06 -0300

    Changed request in getStudentPrograms to Sync

--- requestmaker.js
+++ requestmaker.js
@@ -3,47 +3,43 @@ function requestMaker(baseURL){
     let result={};
 
     result.getStudentPrograms = function() {
-        let request = new XMLHttpRequest();
-        request.open("GET", baseURL + "/api/StudentProgram", true);
+        let request = new XMLHttpRequest();         
+        request.open("GET", baseURL + "/api/StudentProgram", false); //TODO: FIGURE OUT A WAY TO MAKE IT ASYNCHRONUS
         request.send(null);
-        return request.result
+        let data = JSON.parse(request.response);
+        return data;
+
     };
 
     result.postStudentProgram = function(studentProgram) {
         let request = new XMLHttpRequest();
         request.open("POST", baseURL + "/api/StudentProgram", true)
         let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
-        request.send(data);
-        return request.result
+        request.addEventListener("load", request.send(data));   
+        return request.result;
     };
 
     result.patchStudentProgram = function(id, studentProgram) {
         let request = new XMLHttpRequest();
         request.open("POST", baseURL + "/api/StudentProgram/" + id, true);
         request.setRequestHeader("Content-Type", "application/json");
-       /* request.onreadystatechange = function () {
-            if (request.readyState === 4 && request.status === 200) {
-                let json = JSON.parse(request.responseText);
-                console.log(json);
-            }
-        };*/
         let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
-        request.send(data);
-        return request.result
+        request.addEventListener("load", request.send(data));  
+        return request.result;
     };
     
     result.deleteStudentProgram = function(){
         let request = new XMLHttpRequest();
         request.open("DELETE", baseURL + "/api/StudentProgram/", true);
-        request.send(id);
-        return request.result
+        request.addEventListener("load", request.send(data));  
+        return request.result;
     };
 
     result.selectTopProgram = function(){
         let request = new XMLHttpRequest();
-        request.open("DELETE", baseURL + "/api/StudentProgram/Select", true);
-        request.send(null);
-        return request.result
+        request.open("POST", baseURL + "/api/StudentProgram/Select", true);
+        request.addEventListener("load", request.send(null));   
+        return request.result;
     };
     return result;
 }
