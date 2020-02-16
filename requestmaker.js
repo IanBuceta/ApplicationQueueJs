1b9065a0ee5c4443b15fa5bc8331ae4c6dc106d2
IanBuceta <iansbrizza@gmail.com>
2020-02-16 11:09:25 -0300

    Added a separate XMLHttpRequest to every function

--- requestmaker.js
+++ requestmaker.js
@@ -1,22 +1,22 @@
-// TODO: MAKE A REQUEST FOR EVERY FUNCTION. 
 function requestMaker(baseURL){
     let __baseURL = "http://localhost:65072";
-    let request = new XMLHttpRequest();
     let result={};
 
     result.getStudentPrograms = function() {
+        let request = new XMLHttpRequest();
         request.open("GET", baseURL + "/api/StudentProgram", true);
         request.send(null);
     };
 
     result.postStudentProgram = function(studentProgram) {
+        let request = new XMLHttpRequest();
         request.open("POST", baseURL + "/api/StudentProgram", true)
         let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
         request.send(data);
     };
 
     result.patchStudentProgram = function(id, studentProgram) {
-
+        let request = new XMLHttpRequest();
         request.open("POST", baseURL + "/api/StudentProgram/" + id, true);
         request.setRequestHeader("Content-Type", "application/json");
        /* request.onreadystatechange = function () {
@@ -30,11 +30,13 @@ function requestMaker(baseURL){
     };
     
     result.deleteStudentProgram = function(id){
+        let request = new XMLHttpRequest();
         request.open("DELETE", baseURL + "/api/StudentProgram/", true);
         request.send(id);
     };
 
     result.selectTopProgram = function(){
+        let request = new XMLHttpRequest();
         request.open("DELETE", baseURL + "/api/StudentProgram/Select", true);
         request.send(null);
     };
