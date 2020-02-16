9f640c02f8aa44e1fe117fd73cd74f249841c734
IanBuceta <iansbrizza@gmail.com>
2020-02-16 12:17:55 -0300

    Cleaned up requestMaker Function

--- requestmaker.js
+++ requestmaker.js
@@ -1,11 +1,12 @@
 function requestMaker(baseURL){
-    let __baseURL = "http://localhost:65072";
+    let _baseURL = "http://localhost:65072";
     let result={};
 
     result.getStudentPrograms = function() {
         let request = new XMLHttpRequest();
         request.open("GET", baseURL + "/api/StudentProgram", true);
         request.send(null);
+        return request.result
     };
 
     result.postStudentProgram = function(studentProgram) {
@@ -13,6 +14,7 @@ function requestMaker(baseURL){
         request.open("POST", baseURL + "/api/StudentProgram", true)
         let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
         request.send(data);
+        return request.result
     };
 
     result.patchStudentProgram = function(id, studentProgram) {
@@ -27,18 +29,21 @@ function requestMaker(baseURL){
         };*/
         let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
         request.send(data);
+        return request.result
     };
     
-    result.deleteStudentProgram = function(id){
+    result.deleteStudentProgram = function(){
         let request = new XMLHttpRequest();
         request.open("DELETE", baseURL + "/api/StudentProgram/", true);
         request.send(id);
+        return request.result
     };
 
     result.selectTopProgram = function(){
         let request = new XMLHttpRequest();
         request.open("DELETE", baseURL + "/api/StudentProgram/Select", true);
         request.send(null);
+        return request.result
     };
     return result;
 }
