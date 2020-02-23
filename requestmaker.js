57ef55331f46df485a341ace7e4539e2bed47ea5
IanManuel BucetaSbrizza <IanManuel.BucetaSbrizza@Alumnos.uai.edu.ar>
2020-02-23 11:48:37 -0300

    Changed logic of functions

--- requestmaker.js
+++ requestmaker.js
@@ -1,45 +1,90 @@
 function requestMaker(baseURL){
-    let _baseURL = "http://localhost:65072";
     let result={};
 
     result.getStudentPrograms = function() {
-        let request = new XMLHttpRequest();         
-        request.open("GET", baseURL + "/api/StudentProgram", false); //TODO: FIGURE OUT A WAY TO MAKE IT ASYNCHRONUS
-        request.send(null);
-        let data = JSON.parse(request.response);
-        return data;
-
+        return new Promise((resolve,reject)=>{
+            let request = new XMLHttpRequest();         
+            request.open("GET", baseURL + "/api/StudentProgram", true);
+            request.send(null);
+            request.addEventListener("load",()=>{
+                let data = JSON.parse(request.response);
+                resolve(data);
+            });
+        });
     };
 
+    result.getStudentProgram = function(id){
+        return new Promise((resolve,reject)=>{
+            let request = new XMLHttpRequest();       
+            request.open("GET", baseURL + "/api/StudentProgram/" + id, true);
+            request.send(null);
+            request.addEventListener("load",()=>{
+                let data = JSON.parse(request.response);
+                resolve(data);
+            });
+        });
+    }
+
+    function log(data) { console.log(data); }
+
     result.postStudentProgram = function(studentProgram) {
-        let request = new XMLHttpRequest();
-        request.open("POST", baseURL + "/api/StudentProgram", true)
-        let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
-        request.addEventListener("load", request.send(data));   
-        return request.result;
+        return new Promise((resolve,reject)=>{
+            let request = new XMLHttpRequest();
+            request.open("POST", baseURL + "/api/StudentProgram/", true)
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            //request.setRequestHeader('Accept', 'application/json');
+            let data = JSON.stringify({"id": studentProgram.id});// , "teamName": studentProgram.teamName, "src": studentProgram.src, "isRunning": studentProgram.isRunning});
+            request.send(data);
+            request.addEventListener("load", ()=>{
+                resolve(data);
+            });
+            request.addEventListener("error", ()=> {
+                //debugger;
+            });
+        });
+        /*
+        let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src": studentProgram.src, "isRunning": studentProgram.isRunning});
+        console.log(data);
+        return ajax.POST("http://localhost:65072/api/StudentProgram", {"id": studentProgram.id , "teamName": studentProgram.teamName, "src": studentProgram.src, "isRunning": studentProgram.isRunning}).then(log).catch(log);
+        */
     };
 
     result.patchStudentProgram = function(id, studentProgram) {
-        let request = new XMLHttpRequest();
-        request.open("POST", baseURL + "/api/StudentProgram/" + id, true);
-        request.setRequestHeader("Content-Type", "application/json");
-        let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
-        request.addEventListener("load", request.send(data));  
-        return request.result;
+        return new Promise((resolve,reject)=>{
+            let request = new XMLHttpRequest();
+            request.open("POST", baseURL + "/api/StudentProgram/" + id, true);
+            request.setRequestHeader("Content-Type", "application/json");
+            request.setRequestHeader("Accept", "application/json");
+
+            let data = JSON.stringify({"id": studentProgram.id , "teamName": studentProgram.teamName, "src" : studentProgram.src, "isRunning" : studentProgram.isRunning});
+            request.send(data)
+            request.addEventListener("load",()=>{
+                resolve(data);
+            });
+        });
+        
     };
     
     result.deleteStudentProgram = function(){
-        let request = new XMLHttpRequest();
-        request.open("DELETE", baseURL + "/api/StudentProgram/", true);
-        request.addEventListener("load", request.send(data));  
-        return request.result;
+        return new Promise((resolve, reject)=>{
+            let request = new XMLHttpRequest();
+            request.open("DELETE", baseURL + "/api/StudentProgram/", true);
+            request.addEventListener("load", ()=>{
+                resolve(data);
+            }); 
+        });
+        
     };
 
     result.selectTopProgram = function(){
-        let request = new XMLHttpRequest();
-        request.open("POST", baseURL + "/api/StudentProgram/Select", true);
-        request.addEventListener("load", request.send(null));   
-        return request.result;
+        return new Promise((resolve, reject)=>{
+            let request = new XMLHttpRequest();
+            request.open("POST", baseURL + "/api/StudentProgram/Select", true);
+            request.addEventListener("load",()=>{
+                resolve(data);
+            });   
+            
+        });
     };
     return result;
 }
--- scripts.js
+++ scripts.js
@@ -11,50 +11,82 @@ function controller(baseURL){
     
     result.getStudentPrograms = function(){
         let data = request.getStudentPrograms();
-        
-        data.forEach(element => {
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
-
-            const h1 = document.createElement('h1');
-            h1.textContent = element.teamName;
-
-            const p = document.createElement('p');
-            p.textContent = `${element.src}`
-            
-            container.appendChild(card);
-            card.appendChild(h1);
-            card.appendChild(p);
+        data.then((res)=>{
+            res.forEach(element => {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');    
+                const h1 = document.createElement('h1');
+                h1.textContent = element.teamName;
+    
+                const p = document.createElement('p');
+                p.textContent = element.src;
+                
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+            });
         });
+    };
 
-    }
+    result.getStudentProgram = function(id){
+        let data = request.getStudentProgram(id);
+        data.then((element)=>{
+            const card = document.createElement('div');
+
+                card.setAttribute('class', 'card');    
+                const h1 = document.createElement('h1');
+                h1.textContent = element.teamName;
+    
+                const p = document.createElement('p');
+                p.textContent = element.id;
+                const p2 = document.createElement('p');
+                p.textContent = element.isRunning;
+                const p3 = document.createElement('p');
+                p.textContent = element.src;
+                
+                
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+                card.appendChild(p2);
+                card.appendChild(p3);
+        });        
+    };
     
     result.postStudentProgram = function(studentProgram){
-        request.postStudentProgram(studentProgram);
-        result.getStudentPrograms();
-    }
+        let data = request.postStudentProgram(studentProgram);
+        //data.then((element) => {result.getStudentPrograms()});
+    };
 
-    result.patchStudentProgram = function(id, studentProgram){
-        request.patchStudentProgram(id, studentProgram);
-        result.getStudentPrograms();
-
-    }
+    result.patchStudentProgram = function(id, studentProgram){        
+        let data = request.patchStudentProgram(id, studentProgram);
+        data.then((element) => {result.getStudentPrograms()});
+    };
 
     result.deleteStudentProgram = function(){
-        request.deleteStudentProgram();
-        result.getStudentPrograms();
-    }
+        let data = request.deleteStudentProgram();
+        data.then((element) => {result.getStudentPrograms()});        
+    };
 
     result.selectTopProgram = function(){
-        request.selectTopProgram();
-        result.getStudentPrograms();
-    }
+        let data = request.selectTopProgram();
+        data.then((element) => {result.getStudentPrograms()});
+    };
     return result;
 }
 
 let application = controller("http://localhost:65072");
 application.getStudentPrograms();
-//application.addEventListener("",)
+application.postStudentProgram(studentProgram(18,"equipoDeGente", "anda a buscarlo", false));
+/*application.getStudentProgram(1);
+application.deleteStudentProgram();
+application.patchStudentProgram(3, studentProgram(3,"MODIFICADO", "anda a buscarlo", false));*/
 
-//request.open("GET",baseURL+ "/api/StudentProgram", true);
-
+function studentProgram(id, teamName, src, isRunning){
+    let result = {};
+    result.id = id;
+    result.teamName = teamName;
+    result.src = src;
+    result.isRunning = isRunning;
+    return result;
+}
